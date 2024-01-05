05e66d9888859991ebaedda2511faab29d5197a1
Mario Puebla Junior <mariopuebla17@gmail.com>
2024-01-05 14:19:43 -0300

    Update hello.py
    Implementado Hello World em vários idiomas

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+"""Hello World Multi Línguas
+
+Dependendo da língua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+DICAS:
+- Escrever comentários até a coluna 79
+- Usar as regras do site pep8.org para estilização
+"""
+
+__version__ = "0.0.1"
+__author__ = "Mario Puebla"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
